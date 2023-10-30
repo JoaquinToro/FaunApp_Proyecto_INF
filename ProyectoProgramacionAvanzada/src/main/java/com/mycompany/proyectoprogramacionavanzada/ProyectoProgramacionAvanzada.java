eb762063ca2e7d5bffc2b538e86595ef5da17aad
lalalaam <138076810+lalalaam@users.noreply.github.com>
2023-10-30 18:14:55 -0300

    Update ProyectoProgramacionAvanzada.java

--- ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/ProyectoProgramacionAvanzada.java
+++ ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/ProyectoProgramacionAvanzada.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
+import java.util.Scanner
 package com.mycompany.proyectoprogramacionavanzada;
 
 /**
