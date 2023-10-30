5de960023bb17d422a49f05ffa53fc76d4bd8b49
lalalaam <138076810+lalalaam@users.noreply.github.com>
2023-10-30 19:57:34 -0300

    Update ProyectoProgramacionAvanzada.java
    Menu completo,  arreglar si necesario

--- ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/ProyectoProgramacionAvanzada.java
+++ ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/ProyectoProgramacionAvanzada.java
@@ -11,13 +11,15 @@ package com.mycompany.proyectoprogramacionavanzada;
 public class ProyectoProgramacionAvanzada {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola es necesario crear un usuario primero, porfavor crea tu usuario");
         
         Usuario yo = new Usuario("Joaquin Toro","Hombre",21);
         yo.leerDatos();
-        
+        menu();
     }
-    public void menu(){
+    public static void menu(){
+        
+        
         int decision;
         int subdecision;
         Scanner entrada = new Scanner(System.in);
@@ -25,7 +27,7 @@ public class ProyectoProgramacionAvanzada {
         while(true)
         {
             System.out.println("0: Terminar 1:Agregar 2:Mostrar 3:Actualizar/Modificar 4:Eliminar");
-          
+
             decision = entrada.nextInt();
             if(decision == 0)
             {
@@ -34,99 +36,119 @@ public class ProyectoProgramacionAvanzada {
             switch(decision){
                 case 1:
                     System.out.println("A que clase deseas agregar?");
-                    System.out.println("1: Flora 2:Fauna 3:Taxonomia 4:Usuario 5:Region");
+                    System.out.println("1: Flora 2:Fauna 3:Usuario 4:Region");
                     subdecision = entrada.nextInt();
                     switch(subdecision)
                     {
                         case 1:
-                            
+                            FloraInfo newFlora = new FloraInfo();
+                            newFlora.crearDatos();
                             break;
                         case 2:
-                            
+                            FaunaInfo newFauna = new FaunaInfo();
+                            newFauna.crearDatos();
                             break;
                         case 3:
-                            
+                            Usuario newUser = new Usuario();
+                            newUser.crearDatos();
                             break;
                         case 4:
-                            
-                            break;
-                        case 5:
-                            
+                            Region newRegion = new Region();
+                            newRegion.crearDatos();
                             break;
                     }
                     break;
                 case 2:
                     System.out.println("Que clase quieres mostrar?");
-                    System.out.println("1: Flora 2:Fauna 3:Taxonomia 4:Usuario 5:Region");
+                    System.out.println("1: Flora 2:Fauna 3:Usuario 4:Region");
                     subdecision = entrada.nextInt();
                     switch(subdecision)
                     {
                         case 1:
-                            
+                            FloraInfo newFlora = new FloraInfo();
+                            newFlora.mostrarFlora();
                             break;
                         case 2:
-                            
+                            FaunaInfo newFauna = new FaunaInfo();
+                            newFauna.mostrarFauna();
                             break;
                         case 3:
-                            
+                            Usuario newUser = new Usuario();
+                            newUser.mostrarUsuarios();
                             break;
                         case 4:
-                            
+                            Region newRegion = new Region();
+                            newRegion.mostrarRegion();
                             break;
-                        case 5:
-                            
-                            break;
-                        
                     }
                 case 3:
                     System.out.println("Que clase quieres actualizar?");
-                    System.out.println("1: Flora 2:Fauna 3:Taxonomia 4:Usuario 5:Region");
+                    System.out.println("1: Flora 2:Fauna 3:Usuario 4:Region");
                     subdecision = entrada.nextInt();
+                    int idBuscado;
+                    String nombre;
                     switch(subdecision)
                     {
                         case 1:
-                            
+                            System.out.println("Ingrese el id");
+                            idBuscado = entrada.nextInt();
+                            FloraInfo newFlora = new FloraInfo();
+                            newFlora.Actualizar(idBuscado);
                             break;
+
                         case 2:
-                            
+                            System.out.println("Ingrese el id");
+                            idBuscado = entrada.nextInt();
+                            FaunaInfo newFauna = new FaunaInfo();
+                            newFauna.Actualizar(idBuscado);
                             break;
                         case 3:
-                            
+                            System.out.println("Ingrese el nombre de usuario");
+                            nombre = entrada.nextLine();
+                            Usuario newUser = new Usuario();
+                            newUser.Actualizar(nombre);
                             break;
                         case 4:
-                            
-                            break;
-                        case 5:
-                            
+                            System.out.println("Ingrese el id");
+                            idBuscado = entrada.nextInt();
+                            Region newRegion = new Region();
+                            newRegion.Actualizar(idBuscado);
                             break;
                         
                     }
                 case 4:
                     System.out.println("Que clase quieres eliminar?");
-                    System.out.println("1: Flora 2:Fauna 3:Taxonomia 4:Usuario 5:Region");
+                    System.out.println("1: Flora 2:Fauna 3:Usuario 4:Region");
                     subdecision = entrada.nextInt();
                     switch(subdecision)
                     {
                         case 1:
-                            
+                            System.out.println("Ingrese el id");
+                            idBuscado = entrada.nextInt();
+                            FloraInfo newFlora = new FloraInfo();
+                            newFlora.Eliminar(idBuscado);
                             break;
                         case 2:
-                            
+                            System.out.println("Ingrese el id");
+                            idBuscado = entrada.nextInt();
+                            FaunaInfo newFauna = new FaunaInfo();
+                            newFauna.Eliminar(idBuscado);
                             break;
                         case 3:
+                            System.out.println("Ingrese el nombre de usuario");
+                            nombre = entrada.nextLine();
+                            Usuario newUser = new Usuario();
+                            newUser.Eliminar(nombre);
                             
                             break;
                         case 4:
-                            
-                            break;
-                        case 5:
-                            
+                            System.out.println("Ingrese el id");
+                            idBuscado = entrada.nextInt();
+                            Region newRegion = new Region();
+                            newRegion.Eliminar(idBuscado);
                             break;
                         
                     }
-
-
-
             }
         }
     }
