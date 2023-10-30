8301f15150a078bc79db52949d13aa297f7ce0a5
JoaquinToro <136033327+JoaquinToro@users.noreply.github.com>
2023-10-30 18:08:19 -0300

    Update FaunaInfo.java
    Ahora la clase posee herencia de la clase Taxonomia

--- ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/FaunaInfo.java
+++ ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/FaunaInfo.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author Joaquin
  */
-public class FaunaInfo {
+public class FaunaInfo extends Taxonomia{
     //ATRIBUTOS
     private int faunaId;
     private String nombreComun;
@@ -21,35 +21,28 @@ public class FaunaInfo {
     private int peligrosidad;
     private Scanner Entrada = new Scanner(System.in);
     private ArrayList<FaunaInfo> listaFauna = new ArrayList<>();
-    //taxonomia
-    private String division;
-    private String subdivision;
-    private String clase;
-    private String orden;
-    private String familia;
-    private String genero;
-    private String especie;
     
     //Constructor
 
-    public FaunaInfo(int faunaId, String nombreComun, String nombreCientifico, String ubicacion, String alimentacion, int peligrosidad, Scanner Entrada, String division, String subdivision, String clase, String orden, String familia, String genero, String especie) {
+    public FaunaInfo(int faunaId, String nombreComun, String nombreCientifico, String ubicacion, String alimentacion, int peligrosidad, String division, String subdivision, String clase, String orden, String familia, String genero, String especie) {
+        super(division,subdivision,clase,orden,familia,genero,especie);
         this.faunaId = faunaId;
         this.nombreComun = nombreComun;
         this.nombreCientifico = nombreCientifico;
         this.ubicacion = ubicacion;
         this.alimentacion = alimentacion;
         this.peligrosidad = peligrosidad;
-        this.division = division;
-        this.subdivision = subdivision;
-        this.clase = clase;
-        this.orden = orden;
-        this.familia = familia;
-        this.genero = genero;
-        this.especie = especie;
+        
     }
 
-    private FaunaInfo(int faunaId, String nombreComun, String nombreCientifico, String ubicacion, String alimentacion, int peligrosidad, String division, String subdivision, String clase, String orden, String familia, String genero, String especie) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private FaunaInfo(){
+        super(null, null, null, null, null, null, null);
+        this.faunaId = 0;
+        this.nombreComun = null;
+        this.nombreCientifico = null;
+        this.ubicacion = null;
+        this.alimentacion = null;
+        this.peligrosidad = 0;
     }
     
     //getters
@@ -80,34 +73,6 @@ public class FaunaInfo {
 
     public Scanner getEntrada() {
         return Entrada;
-    }
-
-    public String getDivision() {
-        return division;
-    }
-
-    public String getSubdivision() {
-        return subdivision;
-    }
-
-    public String getClase() {
-        return clase;
-    }
-
-    public String getOrden() {
-        return orden;
-    }
-
-    public String getFamilia() {
-        return familia;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public String getEspecie() {
-        return especie;
     }
 
     public ArrayList<FaunaInfo> getListaFauna() {
@@ -144,34 +109,6 @@ public class FaunaInfo {
         this.Entrada = Entrada;
     }
 
-    public void setDivision(String division) {
-        this.division = division;
-    }
-
-    public void setSubdivision(String subdivision) {
-        this.subdivision = subdivision;
-    }
-
-    public void setClase(String clase) {
-        this.clase = clase;
-    }
-
-    public void setOrden(String orden) {
-        this.orden = orden;
-    }
-
-    public void setFamilia(String familia) {
-        this.familia = familia;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-
     public void setListaFauna(ArrayList<FaunaInfo> listaFauna) {
         this.listaFauna = listaFauna;
     }
@@ -185,30 +122,29 @@ public class FaunaInfo {
         this.ubicacion = Entrada.next();
         this.alimentacion = Entrada.next();
         this.peligrosidad = Entrada.nextInt();
-        this.division = Entrada.next();
-        this.subdivision = Entrada.next();
-        this.clase = Entrada.next();
-        this.orden = Entrada.next();
-        this.familia = Entrada.next();
-        this.genero = Entrada.next();
-        this.especie = Entrada.next();
+        this.setDivision(Entrada.next());
+        this.setSubdivision(Entrada.next());
+        this.setClase(Entrada.next());
+        this.setOrden(Entrada.next());
+        this.setFamilia(Entrada.next());
+        this.setGenero(Entrada.next());
+        this.setEspecie(Entrada.next());
         
         listaFauna.add(new FaunaInfo(this.faunaId,this.nombreComun,this.nombreCientifico,
-                                     this.ubicacion,this.alimentacion,this.peligrosidad,
-                                     this.division,this.subdivision,this.clase,this.orden,
-                                     this.familia,this.genero,this.especie));
+                                     this.ubicacion,this.alimentacion,this.peligrosidad,this.getDivision(),this.getSubdivision(),
+                                     this.getClase(), this.getOrden(), this.getFamilia(), this.getGenero(),this.getEspecie()));
     }
     
     //Leer Datos
     public void leerDatos(){
         System.out.println("Información Taxonómica");
-        System.out.println(this.division);
-        System.out.println(this.subdivision);
-        System.out.println(this.clase);
-        System.out.println(this.orden);
-        System.out.println(this.familia);
-        System.out.println(this.genero);
-        System.out.println(this.especie);
+        System.out.println(this.getDivision());
+        System.out.println(this.getSubdivision());
+        System.out.println(this.getClase());
+        System.out.println(this.getOrden());
+        System.out.println(this.getFamilia());
+        System.out.println(this.getGenero());
+        System.out.println(this.getEspecie());
         
         System.out.println("Información General");
         System.out.println(this.nombreCientifico);
