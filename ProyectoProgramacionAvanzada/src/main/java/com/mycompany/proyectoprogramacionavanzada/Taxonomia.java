7492d2e57635762f601864bbdd13ab3f67639966
JoaquinToro <136033327+JoaquinToro@users.noreply.github.com>
2023-10-30 18:09:26 -0300

    Update Taxonomia.java
    Le faltaba especificar el paquete

--- ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/Taxonomia.java
+++ ProyectoProgramacionAvanzada/src/main/java/com/mycompany/proyectoprogramacionavanzada/Taxonomia.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+package com.mycompany.proyectoprogramacionavanzada;
+import java.util.Scanner;
 /**
  *
  * @author usuario
  */
-abstract class Taxonomia {
+public abstract class Taxonomia {
+    //Atributos
     private String division;
     private String subdivision;
     private String clase;
